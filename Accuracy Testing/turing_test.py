4126d6eacca15169a4e2639be6f85f898a311747
ntomita <Naofumi.Tomita.GR@dartmouth.edu>
2019-10-09 13:43:57 -0400

    refactor: remove long lines, simplify conditions.
    *

--- Accuracy Testing/turing_test.py
+++ Accuracy Testing/turing_test.py
@@ -1,24 +1,20 @@
+import argparse
+import csv
 import os
 import random
 import string
-import skimage.measure
-import csv
 import numpy as np
-import argparse
+import skimage.measure
+from os.path import join
 from image_class import image_class
 
 
 # Returns a dictionary with new names like aa.jpg
 def rename(shuffled_keys):
-    # Create the dictionary - key is real image name (e.g. "real/no_alk_123.jpg"), the value is the new name to use (e.g. aa.png)
-    result = {}
-
-    # Add to the dictionary
-    counter = 0
-    for key in shuffled_keys:
-        counter += 1
-        result[key] = str(counter)
-
+    # Create the dictionary
+    # key is real image name (e.g. "real/no_alk_123.jpg")
+    # the value is the new name to use (e.g. aa.png)
+    result = {key: str(i) for i, key in enumerate(shuffled_keys, 1)}
     return result
 
 
@@ -30,33 +26,30 @@ def get_shuffled_keys(dictionary):
 
 
 # Outputs a folder of renamed images, csv's for pathologists to fill out and for real labels
-def generate_turing_test(real_image_folder, synthetic_images_folder, output_folder, control_image_folder, baseline_1, baseline_2):
+def generate_turing_test(real_image_folder, synthetic_images_folder, output_folder,
+                         control_image_folder, baseline_1, baseline_2):
     # Dictionaries to record data
-    real_or_fake_dictionary = {}                # the key is real image name (e.g. "real/no_alk_123.jpg"), the value is "R" or "F" or "C"
-    original_and_renamed_dictionary = {}        # the key is real image name (e.g. "real/no_alk_123.jpg"), the value is the new name to use (e.g. aa.png)
+
+    # the key is real image name (e.g. "real/no_alk_123.jpg")
+    # the value is "R" or "F" or "C"
+    real_or_fake_dictionary = {}
+    # the key is real image name (e.g. "real/no_alk_123.jpg")
+    # the value is the new name to use (e.g. aa.png)
+    original_and_renamed_dictionary = {}
 
     # Loop through real images and register them as real
     for image in os.listdir(real_image_folder):
-        # Get image path
-        image_path = os.path.join(real_image_folder, image)
-
-        # Update dictionary
+        image_path = join(real_image_folder, image)
         real_or_fake_dictionary[image_path] = "R"
 
     # Loop through fake images and register them as fake
     for image in os.listdir(synthetic_images_folder):
-        # Get image path
-        image_path = os.path.join(synthetic_images_folder, image)
-
-        # Update dictionary
+        image_path = join(synthetic_images_folder, image)
         real_or_fake_dictionary[image_path] = "F"
 
     # Loop through control images folder and register them as control
     for image in os.listdir(control_image_folder):
-        # Get image path
-        image_path = os.path.join(control_image_folder, image)
-
-        # Update dictionary
+        image_path = join(control_image_folder, image)
         real_or_fake_dictionary[image_path] = "C"
 
     # Get shuffled keys
@@ -73,12 +66,12 @@ def generate_turing_test(real_image_folder, synthetic_images_folder, output_fold
 
         # Write out each image
         for each in shuffled_keys:
-            if '.DS_Store' not in each:
-                writer.writerow([each, original_and_renamed_dictionary.get(each) + '.png', real_or_fake_dictionary.get(each)])
+            if '.DS_Store' in each:
+                continue
+            writer.writerow([each, original_and_renamed_dictionary.get(each) + '.png', real_or_fake_dictionary.get(each)])
 
     # Create output folder
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
+    os.makedirs(output_folder, exist_ok=True)
 
     # Write pathologist csv
     with open('pathologist_file.csv', mode='w') as file:
@@ -91,13 +84,16 @@ def generate_turing_test(real_image_folder, synthetic_images_folder, output_fold
             writer.writerow([original_and_renamed_dictionary.get(each) + '.png', '', ''])
 
             # Save the image
-            if '.DS_Store' not in each:
-                os.system("cp -r " + each + " " + os.path.join(output_folder, original_and_renamed_dictionary.get(each) + '.png'))
+            if '.DS_Store' in each:
+                continue
+            os.system("cp -r {} {}.png".format(
+                each, join(output_folder, original_and_renamed_dictionary.get(each))))
 
 
 # Whether an image is mostly whitespace, takes in np array
 def is_whitespace(crop):
-    pooled = skimage.measure.block_reduce(crop, (int(crop.shape[0]/10), int(crop.shape[1]/10), 3), np.average)
+    pooled = skimage.measure.block_reduce(
+        crop, (int(crop.shape[0]/10), int(crop.shape[1]/10), 3), np.average)
     pooled = np.mean(pooled, axis=2)
     pooled = np.rint(pooled[:9, :9])
     num_good_squares = 0
@@ -109,7 +105,8 @@ def is_whitespace(crop):
     return True
 
 
-# Gets N images from the folder (synthetic images should have the same name as real images they were generated from but be in different folder)
+# Gets N images from the folder (synthetic images should have the same name as
+# real images they were generated from but be in different folder)
 def get_best_images(input_folder_real, input_folder_synthetic, num_images, output_folder):
     # Count the number of images that have been saved
     counter = 1
@@ -129,8 +126,8 @@ def get_best_images(input_folder_real, input_folder_synthetic, num_images, outpu
             continue
 
         # Full path of real image and synthetic image
-        current_image_path_real = os.path.join(input_folder_real, image)
-        current_image_path_fake = os.path.join(input_folder_synthetic, image)
+        current_image_path_real = join(input_folder_real, image)
+        current_image_path_fake = join(input_folder_synthetic, image)
 
         # Load images
         current_image_real = image_class(current_image_path_real)
@@ -140,27 +137,44 @@ def get_best_images(input_folder_real, input_folder_synthetic, num_images, outpu
         if counter > num_images:
             return
 
-        current_image_fake.save_image(os.path.join(output_folder, image))
+        current_image_fake.save_image(join(output_folder, image))
         counter += 1
 
 
 if __name__ == "__main__":
-
     parser = argparse.ArgumentParser()
-    parser.add_argument("--real_image_folder", type=str, help="path to the folder with real images")
-    parser.add_argument("--fake_image_folder", type=str, help="path to the folder with fake images")
-    parser.add_argument("--output_folder", type=str, help="where to output the renamed images")
-    parser.add_argument("--num_images", type=int, help="N number of images to use per class (total of 2N images)")
-    parser.add_argument("--operation_num", type=int, help="0 to generate Turing Test, 1 to get N images from folder")
-    parser.add_argument("--control_image_folder", type=str, help="path to the folder with control images")
-    parser.add_argument("--baseline_1_folder", type=str, help="path to the folder with first baseline images")
-    parser.add_argument("--baseline_2_folder", type=str, help="path to the folder with second baseline images")
+    parser.add_argument("--real_image_folder", type=str,
+                        help="path to the folder with real images")
+    parser.add_argument("--fake_image_folder", type=str,
+                        help="path to the folder with fake images")
+    parser.add_argument("--output_folder", type=str,
+                        help="where to output the renamed images")
+    parser.add_argument("--num_images", type=int,
+                        help="N number of images to use per class (total of 2N images)")
+    parser.add_argument("--operation_num", type=int,
+                        help="0 to generate Turing Test, 1 to get N images from folder")
+    parser.add_argument("--control_image_folder", type=str,
+                        help="path to the folder with control images")
+    parser.add_argument("--baseline_1_folder", type=str,
+                        help="path to the folder with first baseline images")
+    parser.add_argument("--baseline_2_folder", type=str,
+                        help="path to the folder with second baseline images")
     args = parser.parse_args()
 
     if args.operation_num is 1:
-        get_best_images(args.real_image_folder, args.fake_image_folder, args.num_images, args.output_folder)
+        get_best_images(
+            args.real_image_folder,
+            args.fake_image_folder,
+            args.num_images,
+            args.output_folder)
     elif args.operation_num is 0:
-        generate_turing_test(args.real_image_folder, args.fake_image_folder, args.output_folder, args.control_image_folder, args.baseline_1_folder, args.baseline_2_folder)
+        generate_turing_test(
+            args.real_image_folder,
+            args.fake_image_folder,
+            args.output_folder,
+            args.control_image_folder,
+            args.baseline_1_folder,
+            args.baseline_2_folder)
     else:
         print("Invalid Operation")
-        exit()
+        exit(1)
